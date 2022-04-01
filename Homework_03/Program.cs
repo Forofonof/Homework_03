b69a9c987158e664da0d91fcc63771d6929ee023
Forofonof <DanielResult1@outlook.com>
2022-04-01 18:57:47 +0300

    Добавьте файлы проекта.

---
+++ Homework_03/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        int picturesTotal = 52;
+        int picturesInRow = 3;
+        int picturesRows = picturesTotal / picturesInRow;
+        int picturesRemainder = picturesTotal % picturesInRow;
+        Console.WriteLine($"Картинок в ряду: {picturesRows}. Остаток: {picturesRemainder}.");
+    }
+}
